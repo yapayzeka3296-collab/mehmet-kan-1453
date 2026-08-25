20304c63c7e706e7fc500f89c94a232d5dec4666
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-08-25 16:42:47 +0300

    Remove Google naming from focused parcel map

---
+++ src/components/FocusedSkyParcelMap.tsx
@@ -0,0 +1,9 @@
+import { SkyParcelMap } from "@/components/SkyParcelMap";
+import type { Parcel } from "@/types/parcel";
+
+type CityCenter={lat:number;lng:number};
+type ViewportBounds={minLat:number;minLng:number;maxLat:number;maxLng:number};
+type FocusTarget={city:CityCenter;parcel:CityCenter;token:string};
+type Props={parcels:Parcel[];selectedId:string|null;selectedIds?:Set<string>;multiSelect?:boolean;onSelect:(id:string|null)=>void;onToggleSelect?:(id:string)=>void;onViewportChange?:(bounds:ViewportBounds)=>void;center:CityCenter;focusTarget?:FocusTarget|null};
+
+export function FocusedSkyParcelMap(props: Props) { return <SkyParcelMap {...props} />; }
