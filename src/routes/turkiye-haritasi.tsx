a6e35944d1d35eb3d9f023382691e605ee112c36
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-08-27 02:39:36 +0300

    Revert last Turkey map route change

--- src/routes/turkiye-haritasi.tsx
+++ src/routes/turkiye-haritasi.tsx
@@ -1,14 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SiteHeader } from "@/components/SiteHeader";
-import { TurkeyProvinceMapView } from "@/components/TurkeyProvinceMapView";
+import { LegacySkyMapView } from "@/components/LegacySkyMapView";
 
-export const Route = createFileRoute("/turkiye-haritasi")({ component: TurkeyMapPage });
+export const Route = createFileRoute("/turkiye-haritasi")({ component: SkyMapPage });
 
-function TurkeyMapPage() {
-  return (
-    <div className="min-h-screen bg-slate-950 text-white">
-      <SiteHeader />
-      <TurkeyProvinceMapView />
-    </div>
-  );
+function SkyMapPage() {
+  return <div className="min-h-screen bg-slate-950 text-white"><SiteHeader/><LegacySkyMapView/></div>;
 }
