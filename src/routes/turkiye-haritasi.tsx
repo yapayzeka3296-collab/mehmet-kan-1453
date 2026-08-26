e2825c4777f7d40b020876795b06fdba6fd3c512
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-08-26 19:19:01 +0300

    Restore standalone Sky Map page layout

--- src/routes/turkiye-haritasi.tsx
+++ src/routes/turkiye-haritasi.tsx
@@ -1,8 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { SiteHeader } from "@/components/SiteHeader";
 import { LegacySkyMapView } from "@/components/LegacySkyMapView";
 
 export const Route = createFileRoute("/turkiye-haritasi")({ component: SkyMapPage });
 
 function SkyMapPage() {
-  return <LegacySkyMapView />;
+  return <div className="min-h-screen bg-slate-950 text-white"><SiteHeader/><LegacySkyMapView/></div>;
 }
