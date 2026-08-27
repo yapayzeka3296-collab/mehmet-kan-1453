2207ffb0ba044fbcf4a6cad79104b2883475e9b6
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-08-27 12:56:02 +0300

    Use controlled sky parcel map for city pages

--- src/routes/sehir/$slug.tsx
+++ src/routes/sehir/$slug.tsx
@@ -1,10 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SiteHeader } from "@/components/SiteHeader";
-import { CityParcelGridPage } from "@/components/CityParcelGridPage";
+import { SkyParcelCityMapPage } from "@/components/SkyParcelCityMapPage";
 
 export const Route = createFileRoute("/sehir/$slug")({ component: CityPage });
 
 function CityPage() {
   const { slug } = Route.useParams();
-  return <div className="min-h-screen bg-slate-950 text-white"><SiteHeader /><CityParcelGridPage slug={slug} /></div>;
+  return <div className="min-h-screen bg-slate-950 text-white"><SiteHeader /><SkyParcelCityMapPage slug={slug} /></div>;
 }
