6e21cf98c753a8b04c4883dcf97598d41a532597
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-09-04 23:31:23 +0300

    fix: remove obsolete ile route

--- src/routes/ile.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/ile")({
-  beforeLoad: () => {
-    throw redirect({ to: "/iletisim", replace: true });
-  },
-});
