c849050e1a26b2f57d55d2e89f54478d70ef61aa
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-09-10 19:42:06 +0300

    fix: integrate resilient sky scan sensor bridge

--- src/routes/gokyuzu-tara.tsx
+++ src/routes/gokyuzu-tara.tsx
@@ -1,4 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SkyScanExperienceV3 } from "@/components/sky-scan/SkyScanExperienceV3";
+import { SkyScanSensorBridge } from "@/components/sky-scan/SkyScanSensorBridge";
 
-export const Route = createFileRoute("/gokyuzu-tara")({ component: SkyScanExperienceV3 });
+function SkyScanPage() {
+  return (
+    <SkyScanSensorBridge>
+      <SkyScanExperienceV3 />
+    </SkyScanSensorBridge>
+  );
+}
+
+export const Route = createFileRoute("/gokyuzu-tara")({ component: SkyScanPage });
