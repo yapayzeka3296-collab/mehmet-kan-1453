1d325b24b9f27e0257b570d49edac9543113cec3
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-09-11 17:05:15 +0300

    fix: route sky scan to resilient compass V8

--- src/routes/gokyuzu-tara.tsx
+++ src/routes/gokyuzu-tara.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SkyScanExperienceV7 } from "@/components/sky-scan/SkyScanExperienceV7";
+import { SkyScanExperienceV8 } from "@/components/sky-scan/SkyScanExperienceV8";
 
 function SkyScanRoute() {
-  return <SkyScanExperienceV7 />;
+  return <SkyScanExperienceV8 />;
 }
 
 export const Route = createFileRoute("/gokyuzu-tara")({ component: SkyScanRoute });
