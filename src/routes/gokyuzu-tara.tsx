e70c6dff86ce024d2eeb05714c7e1edd92c57860
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-09-11 14:35:09 +0300

    fix: route sky scan to camera-only experience

--- src/routes/gokyuzu-tara.tsx
+++ src/routes/gokyuzu-tara.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SkyScanExperienceV6 } from "@/components/sky-scan/SkyScanExperienceV6";
+import { SkyScanExperienceV5 } from "@/components/sky-scan/SkyScanExperienceV5";
 
 function SkyScanRoute() {
-  return <SkyScanExperienceV6 />;
+  return <SkyScanExperienceV5 />;
 }
 
 export const Route = createFileRoute("/gokyuzu-tara")({ component: SkyScanRoute });
