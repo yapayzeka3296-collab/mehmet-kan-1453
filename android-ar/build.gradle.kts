16b95d98c02eb39fccc30ff4d97559e910c8c3ce
yapayzeka3296-collab <yapayzeka3296@gmail.com>
2026-09-13 03:28:18 +0300

    fix(ar): align Kotlin with current SceneView toolchain

--- android-ar/build.gradle.kts
+++ android-ar/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
